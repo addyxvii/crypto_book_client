9c9dd14ab1dda921102e9d6ee1b302e8ed505ecc
addyxvii <addisonbennett@gmail.com>
2018-08-15 19:07:54 -0600

    switched to recharts

--- src/components/orders.js
+++ src/components/orders.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
 import {
-    Charts,
-    ChartContainer,
-    ChartRow,
+    AreaChart, 
+    linearGradient, 
+    CartesianGrid, 
+    Area, 
+    defs,
+    XAxis,
     YAxis,
-    LineChart,
-} from "react-timeseries-charts";
+    Tooltip,
+} from "recharts";
 
 class Orders extends React.Component {
     render() {
         return(
-        <ChartContainer timeRange={timeRange()} width={800}>
-        <ChartRow height="200">
-            <YAxis id="axis1" label="AUD" min={0.5} max={1.5} width="60" type="linear" format="$,.2f"/>
-            <Charts>
-                <LineChart axis="axis1" series={[1, 2, 3]} column={["aud"]}/>
-                <LineChart axis="axis2" series={[3, 4, 5]} column={["euro"]}/>
-            </Charts>
-            <YAxis id="axis2" label="Euro" min={0.5} max={1.5} width="80" type="linear" format="$,.2f"/>
-        </ChartRow>
-    </ChartContainer>
+            <AreaChart width={730} height={250} data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]}
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+                <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+              </linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+            <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+          </AreaChart>
         )}
 };
 export default(Orders); 
