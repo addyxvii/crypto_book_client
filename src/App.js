61da09c44544b78df03a097b410c5eb19e029fe0
addyxvii <addisonbennett@gmail.com>
2018-08-15 21:08:56 -0600

    removed comment

--- src/App.js
+++ src/App.js
@@ -3,27 +3,6 @@ import './App.css';
 import Orders from './components/orders.js';
 
 class App extends Component {
-  // constructor(props){
-  //   super(props);
-  //   this.state = {data : 'no data yet' }
-  // };
-
-  // componentDidMount() {
-  //   getBittrex((error, data) => {
-  //     console.log('component has socket data');
-  //     this.setState({
-  //      bittrex : data,
-  //     })
-  //   });
-
-  //   getPoloniex((error, data) => {
-  //     console.log('component has Poloniex data')
-  //     this.setState({
-  //       poloniex : data,
-  //     })
-  //   });
-  // }
-
   render() {
     return (
      <Orders />
